8d5065a369dddd4aea7d929932e9e6c15c890670
Nicholas Dunn <nexela@gmail.com>
2016-09-22 01:56:25 -0400

    tailings pond working

--- data-updates.lua
+++ data-updates.lua
@@ -1,3 +1,3 @@
 --require("util")  --require some helper functions
 --require("config") -- require our config values from config.lua
-require("prototypes.recipes.recipes_updates.lua") -- The recipes we want to update.
+require("prototypes.recipes.recipe-updates") -- The recipes we want to update.
