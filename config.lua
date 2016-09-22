22175be62bb5823a866a83e43e5fb84ce6214626
Nicholas Dunn <nexela@gmail.com>
2016-09-22 13:28:21 -0400

    Ground pollution is in
    adds in ground pollution
    walking is slowed on polluted ground
    big wooden power pole added to creosote recipes
    ground pollution tweaked some to keep down update times, still needs some
    more tweaking in regards to randomness on placement
    
    
    Former-commit-id: f299bbff6fdc07741f27c1e671119623ddc29b21 [formerly 13c95350f561a98acb7bf0e1fd1d08b82646fbae]
    Former-commit-id: f299bbff6fdc07741f27c1e671119623ddc29b21
    Former-commit-id: 52dd2663da262c5548027cc9b72074dbff7c67ac

--- config.lua
+++ config.lua
@@ -1,23 +1,30 @@
 --luacheck: ignore
-
-USECREOSOTE_RAIL = true
-USECREOSOTE_POLE = true
-
 PYC = {}
+PYC.TAILINGS_POND = {}
 
---Config settings for tailings ponds
-TAILINGS_POND = {}
-
---Tank size, how much of a fluid the tank can hold
-TAILINGS_POND.TANK_SIZE = 5000
-
---Does gas entering the tailings pond cause pollution when vented
---default=.15 off=0
-TAILINGS_POND.GAS_POLLUTE_MODIFIER = .15
-
---Destroying a tailings pond with non water liquids will change the terrain
---Disabled for now until it can be refined some more
-TAILINGS_POND.SCORCH = false
 
 --Messages and stuff for developers
 PYC.DEBUG = true
+
+--Changes some vanilla/mod recipes to use Creosate
+--USE_CREOSOTE_RECIPES, if false will disable use of creosote in vanilla recipes. if true
+--then any recipes that are true in USE_CREOSOTE_IN will use creosote.
+PYC.USE_CREOSOTE_RECIPES = true
+PYC.USE_CREOSOTE_IN = {
+  --Rail tracks
+  rail = true,
+  --Medium power pole, big wooden power pole
+  power_poles = true,
+}
+
+--Tank size, how much of a fluid the tank can hold
+PYC.TAILINGS_POND.TANK_SIZE = 5000
+
+--Does gas entering the tailings pond cause pollution when vented
+--default=.15 off=0
+PYC.TAILINGS_POND.GAS_POLLUTE_MODIFIER = .15
+
+--Tailings ponds that contain non water liquids pollute the ground they are on
+--Disabled for now until it can be refined some more
+PYC.TAILINGS_POND.SCORCH = true
+PYC.TAILINGS_POND.SCORCH_TICKS = 30 --default: 360?
