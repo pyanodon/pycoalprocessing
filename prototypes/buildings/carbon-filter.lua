13257a43426763d667448058967bc455d2382e61
Nicholas Dunn <nexela@gmail.com>
2016-10-10 06:05:36 -0400

    Adjust pipes for carbon-filter
    Adjust pipes for carbon filter
    fixes #7
    small tweak to distilator connections
    move asphalt gfx to folder
    
    
    Former-commit-id: 4d2dfbc02d46ef5fa5c7c71bf50d821aeb377079 [formerly e8b8953f34b9459ceda1ed0206ee41369ded485a]
    Former-commit-id: 4d2dfbc02d46ef5fa5c7c71bf50d821aeb377079
    Former-commit-id: 8bdbe9980582e4d36f199ad34d752244c46f517c

--- prototypes/buildings/carbon-filter.lua
+++ prototypes/buildings/carbon-filter.lua
@@ -1,3 +1,36 @@
+local pipe_pictures = function(shift_north, shift_south)
+  local north, south, east, west
+  if shift_north then
+    north =
+    {
+      filename = "__pycoalprocessing__/graphics/entity/carbon-filter/pipe-north.png",
+      priority = "extra-high",
+      width = 44,
+      height = 32,
+      --shift = {0.03125, 0.3125}
+      shift = shift_north
+    }
+  else
+    north = Proto.empty_sprite
+  end
+  if shift_south then
+    south =
+    {
+      filename = "__base__/graphics/entity/assembling-machine-2/pipe-south.png",
+      priority = "extra-high",
+      width = 41,
+      height = 40,
+      --shift = {0.03125, -1.0625}
+      shift = shift_south
+    }
+  else
+    south = Proto.empty_sprite
+  end
+    west = Proto.empty_sprite
+    east = Proto.empty_sprite
+  return {north=north, south=south, west=west, east=east}
+end
+
 -------------------------------------------------------------------------------
 --[[Recipes]]--
 local recipe1={
@@ -40,7 +73,7 @@ local entity1={
   max_health = 150,
   corpse = "big-remnants",
   dying_explosion = "medium-explosion",
-  collision_box = {{-2.4, -2.4}, {2.4, 2.4}},
+  collision_box = {{-2.25, -2.25}, {2.25, 2.25}},
   selection_box = {{-2.5, -2.5}, {2.5, 2.5}},
   module_specification =
   {
@@ -72,22 +105,23 @@ local entity1={
   {
     {
       production_type = "input",
-      pipe_picture = Proto.pipes("assembler", {0.1, 0.40}, {-0.00, -0.75}, {0.55, 0.15}, {-0.55, 0.15}),
-      pipe_covers = Proto.pipe_covers(false, true, true, true),
+      --pipe_picture = Proto.pipes("assembler", {0.1, 0.40}, {-0.00, -0.75}, {0.55, 0.15}, {-0.55, 0.15}),
+      pipe_picture = pipe_pictures({0.0, 0.65}, {0.05, -0.9}),
+      pipe_covers = Proto.pipe_covers(true, true, true, true),
       base_area = 10,
       base_level = -1,
-      pipe_connections = {{ type="input", position = {2.9, -0.0} }}
+      pipe_connections = {{ type="input", position = {3.0, -0.0} }}
     },
 
     {
       production_type = "output",
-      pipe_picture = Proto.pipes("assembler", {0.1, 0.40}, {-0.00, -0.75}, {0.55, 0.15}, {-0.55, 0.15}),
-      pipe_covers = Proto.pipe_covers(false, true, true, true),
+      --pipe_picture = Proto.pipes("assembler", {0.1, 0.40}, {-0.00, -0.75}, {0.55, 0.15}, {-0.55, 0.15}),
+      pipe_picture = pipe_pictures({0.0, 0.65}, {0.05, -0.9}),
+      pipe_covers = Proto.pipe_covers(true, true, true, true),
       base_level = 1,
-      pipe_connections = {{ position = {-2.9, -0.0} }}
+      pipe_connections = {{ position = {-3.0, -0.0} }}
     },
   },
-  pipe_covers = Proto.pipe_covers(true, true, true, true),
   vehicle_impact_sound = { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
   working_sound =
   {
