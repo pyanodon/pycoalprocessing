b7efd8dd92f49374894068adebe1d833c97605d6
Nicholas Dunn <nexela@gmail.com>
2016-10-11 03:10:01 -0400

    Adjust rectisol pipe connections Not perfect but passable fixes #11

--- prototypes/buildings/rectisol.lua
+++ prototypes/buildings/rectisol.lua
@@ -1,3 +1,56 @@
+local pipe_pictures = function(shift_north, shift_south, shift_west, shift_east)
+  local north, south, east, west
+  if shift_north then
+    north =
+    {
+      filename = "__pycoalprocessing__/graphics/entity/rectisol/pipe-north.png",
+      priority = "extra-high",
+      width = 44,
+      height = 32,
+      shift = shift_north
+    }
+  else
+    north = Proto.empty_sprite
+  end
+  if shift_south then
+    south =
+    {
+      filename = "__pycoalprocessing__/graphics/entity/rectisol/pipe-ending-down.png",
+      priority = "extra-high",
+      width = 40,
+      height = 45,
+      shift = shift_south
+    }
+  else
+    south = Proto.empty_sprite
+  end
+  if shift_west then
+    west =
+    {
+      filename = "__base__/graphics/entity/assembling-machine-3/pipe-west.png",
+      priority = "extra-high",
+      width = 40,
+      height = 45,
+      shift = shift_west
+    }
+  else
+    west = Proto.empty_sprite
+  end
+  if shift_east then
+    east =
+    {
+      filename = "__base__/graphics/entity/assembling-machine-3/pipe-east.png",
+      priority = "extra-high",
+      width = 40,
+      height = 45,
+      shift = shift_east
+    }
+  else
+    east = Proto.empty_sprite
+  end
+  return {north=north, south=south, west=west, east=east}
+end
+
 -------------------------------------------------------------------------------
 --[[Recipes]]--
 local recipe1={
@@ -107,16 +160,16 @@ local entity1={
   {
     {
       production_type = "input",
-      --pipe_picture = floatationpipepictures(),
-      pipe_covers = Proto.pipe_covers(true, true, true, true),
+      pipe_picture = pipe_pictures({0.0, 0.65}, {-0.05, -0.8}, nil, nil),
+      pipe_covers = Proto.pipe_covers(false, true, true, true),
       base_area = 10,
       base_level = -1,
       pipe_connections = {{ type="input", position = {4.0, -1.0} }}
     },
     {
       production_type = "input",
-      --pipe_picture = floatationpipepictures(),
-      pipe_covers = Proto.pipe_covers(true, true, true, true),
+      pipe_picture = pipe_pictures({0.0, 0.65}, {-0.05, -0.8}, nil, nil),
+      pipe_covers = Proto.pipe_covers(false, true, true, true),
       base_area = 10,
       base_level = -1,
       pipe_connections = {{ type="input", position = {4.0, 1.0} }}
@@ -124,29 +177,29 @@ local entity1={
 
     {
       production_type = "output",
-      --pipe_picture = floatationpipepictures()
-      pipe_covers = Proto.pipe_covers(true, true, true, true),
+      pipe_picture = pipe_pictures({0.0, 0.65}, {-0.05, -0.8}, nil, nil),
+      pipe_covers = Proto.pipe_covers(false, true, true, true),
       base_level = 1,
       pipe_connections = {{ position = {-4.0, -1.0} }}
     },
     {
       production_type = "output",
-      --pipe_picture = floatationpipepictures()
-      pipe_covers = Proto.pipe_covers(true, true, true, true),
+      pipe_picture = pipe_pictures({0.0, 0.65}, {-0.05, -0.8}, nil, nil),
+      pipe_covers = Proto.pipe_covers(false, true, true, true),
       base_level = 1,
       pipe_connections = {{ position = {-4.0, -3.0} }}
     },
     {
       production_type = "output",
-      --pipe_picture = floatationpipepictures()
-      pipe_covers = Proto.pipe_covers(true, true, true, true),
+      pipe_picture = pipe_pictures({0.0, 0.65}, {-0.05, -0.8}, nil, nil),
+      pipe_covers = Proto.pipe_covers(false, true, true, true),
       base_level = 1,
       pipe_connections = {{ position = {-4.0, 1.0} }}
     },
     {
       production_type = "output",
-      --pipe_picture = floatationpipepictures()
-      pipe_covers = Proto.pipe_covers(true, true, true, true),
+      pipe_picture = pipe_pictures({0.0, 0.65}, {-0.05, -0.8}, nil, nil),
+      pipe_covers = Proto.pipe_covers(false, true, true, true),
       base_level = 1,
       pipe_connections = {{ position = {-4.0, 3.0} }}
     },
