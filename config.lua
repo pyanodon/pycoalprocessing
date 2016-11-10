478bfa40a88b9f94ce42f73460c48c363ae81510
Nicholas Dunn <nexela@gmail.com>
2016-11-10 15:05:19 -0500

    Reset config values closes #17

--- config.lua
+++ config.lua
@@ -4,8 +4,8 @@ PYC.TAILINGS_POND = {} -- Do not change
 
 --Messages and stuff for developers
 --These should be false in a release.
-PYC.DEBUG = true
-PYC.DEBUG_QUICKSTART = true
+PYC.DEBUG = false
+PYC.DEBUG_QUICKSTART = false
 
 --Changes some vanilla/mod recipes to use Creosate
 --USE_CREOSOTE_RECIPES, if false will disable use of creosote in vanilla recipes. if true
@@ -29,7 +29,7 @@ PYC.TAILINGS_POND.GAS_POLLUTE_MODIFIER = .15
 --chance is a percentage from 1 to 100, set to 0 to disable.
 --ticks is the number of ticks that must pass with a full tank before another check is done.
 PYC.TAILINGS_POND.SCORCH_CHANCE = 3 --default 3
-PYC.TAILINGS_POND.SCORCH_TICKS = 3600 --default: 3600 (this will check every 1 minutes)
+PYC.TAILINGS_POND.SCORCH_TICKS = 36000 --default: 36000 (this will check every 10 minutes)
 
 
 --List of Gas fluids, true values cause pollution when vented, all gasses with gas in the name fall under this category
