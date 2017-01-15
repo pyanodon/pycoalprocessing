e4c274e356a94670ae4d35e70b5372d09b027e0e
Nexela <nexela@gmail.com>
2017-01-15 04:17:48 -0500

    More tweaks, no errors Still need to check generators and adjust output.
    Former-commit-id: 277c435e516bb52dd1dd9074fea95e4b2c9e76c6

--- interface.lua
+++ interface.lua
@@ -1,7 +1,12 @@
+require("stdlib/config/config")
 local interface = {}
 
 function interface.reset_mod()
   game.raise_event(Event.reset_mod, {})
+end
+
+function interface.reset_config()
+  interface.config("reset")
 end
 
 function interface.resetUser()
@@ -10,12 +15,8 @@ end
 function interface.recoverItems()
 end
 
---Require the awesome console for quick script debugging
-interface.console = require("stdlib.utils.console")
-
-
 function interface.print_global(name) --Dumps the global to player and logfile
-  if name then
+  if name and type(name) == "string" then
     game.print(serpent.block(global[name], {comment=false, sparse=true}))
     game.write_file("/logs/"..MOD.name .. "/global.log", serpent.block(global[name], {comment=false, sparse=true}))
   else
@@ -24,38 +25,48 @@ function interface.print_global(name) --Dumps the global to player and logfile
   end
 end
 
--- function interface.config(key, value, silent)
---   if key then
---     --key=string.upper(key)
---     if MOD.config.get(key) ~= nil then
---       if value ~= nil then
---         MOD.config.set(key, value)
---         local val=MOD.config.get(key)
---         if not silent then game.print(MOD.IF .. ": New value for '" .. key .. "' is " .. "'" .. tostring(val) .."'") end
---         return val-- all is well
---       else --value nil
---         local val = MOD.config.get(key)
---         if not silent then game.print(MOD.IF .. ": Current value for '" .. key .. "' is " .. "'" .. tostring(val) .."'") end
---         return val
---       end
---     else --key is nill
---       if not silent then game.print(MOD.IF ..": Config '" .. key .. "' does not exist") end
---       return nil
---     end
---   else
---     if not silent then
---       game.print(MOD.IF .. ": Config requires a key name")
---       game.print(serpent.line(MOD.config))
---     end
---     return nil
---   end
--- end
+function interface.config(key, value, silent)
+  local config = Config.new(global.config)
+  if key then
+    if key == "reset" then
+      global.config = MOD.config.control
+      if not silent then game.print(MOD.name.." Reset config to default.") end
+      return true
+    end
+    --key=string.upper(key)
+    if config.get(key) ~= nil then
+      if value ~= nil then
+        config.set(key, value)
+        local val=config.get(key)
+        if not silent then game.print(MOD.name .. ": New value for '" .. key .. "' is " .. "'" .. tostring(val) .."'") end
+        return val-- all is well
+      else --value nil
+        local val = config.get(key)
+        if not silent then game.print(MOD.name .. ": Current value for '" .. key .. "' is " .. "'" .. tostring(val) .."'") end
+        return val
+      end
+    else --key is nill
+      if not silent then game.print(MOD.name ..": Config '" .. key .. "' does not exist") end
+      return nil
+    end
+  else
+    if not silent then
+      game.print(MOD.name .. ": Config requires a key name")
+      game.print(serpent.line(global.config))
+    end
+    return nil
+  end
+end
+
+--Require the awesome console for quick script debugging
+interface.console = require("stdlib.utils.console")
 
 --Register with creative-mode for easy testing
 if remote.interfaces["creative-mode"] and remote.interfaces["creative-mode"]["register_remote_function_to_modding_ui"] then
   remote.call("creative-mode", "register_remote_function_to_modding_ui", "PYC", "print_global")
   remote.call("creative-mode", "register_remote_function_to_modding_ui", "PYC", "reset_mod")
-  if interface.console then remote.call("creative-mode", "register_remote_function_to_modding_ui", "PYC", "console") end
+  remote.call("creative-mode", "register_remote_function_to_modding_ui", "PYC", "reset_user")
+  remote.call("creative-mode", "register_remote_function_to_modding_ui", "PYC", "console")
 end
 
 return interface
