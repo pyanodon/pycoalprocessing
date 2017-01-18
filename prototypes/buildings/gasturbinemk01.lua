c5cc12129ea9446ad302da72d1c111a95ebed99b
Nexela <nexela@gmail.com>
2017-01-17 21:31:43 -0500

    power tweaks

--- prototypes/buildings/gasturbinemk01.lua
+++ prototypes/buildings/gasturbinemk01.lua
@@ -38,8 +38,8 @@ local entity1={
   minable = {mining_time = 1, result = "gasturbinemk01"},
   max_health = 300,
   corpse = "big-remnants",
-  effectivity = 200,
-  fluid_usage_per_tick = 0.005, --set via script
+  effectivity = 10,
+  fluid_usage_per_tick = 0.05,
   resistances =
   {
     {
