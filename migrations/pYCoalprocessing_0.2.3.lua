de4efa9ead6620d730d6f348a8fff482c3cbbdde
Nexela <nexela@gmail.com>
2017-01-22 23:10:20 -0500

    Fixed error in migration Tweaks to makefile. Remove un-needed files.
    Former-commit-id: a33cfb580de2e97f36a720b27e836c18dcd07257 [formerly ca063e915f0bc7c785c77143146fbc18458e9fbd]
    Former-commit-id: a33cfb580de2e97f36a720b27e836c18dcd07257
    Former-commit-id: 79588f075f4343c5731236f42e5349a259261ec9

--- migrations/pYCoalprocessing_0.2.3.lua
+++ migrations/pYCoalprocessing_0.2.3.lua
@@ -1,5 +1,5 @@
 for _, force in pairs(game.forces) do
   if force.technologies["py-asphalt"].researched then
-    data.raw.recipe["py-coal-tile"].enabled = true
+    force.recipes["py-coal-tile"].enabled = true
   end
 end
