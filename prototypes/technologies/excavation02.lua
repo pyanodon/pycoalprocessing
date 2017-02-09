efb6400385b3668b2aef2966614074dedaba2a46
pyanodon <smylodon@gmail.com>
2017-02-09 18:58:07 -0200

    More recipes
    Research and more recipes to ground borer

---
+++ prototypes/technologies/excavation02.lua
@@ -0,0 +1,75 @@
+local technology = {
+  type = "technology",
+  name = "excavation02",
+  icon = "__pycoalprocessing__/graphics/technology/borer02.png",
+  icon_size = 128,
+  order = "c-a",
+  prerequisites = {"excavation01"},
+  effects =
+  {
+
+	{
+		type = "unlock-recipe",
+		recipe = "mining-bauxite"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "mining-cobalt"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "mining-gold"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "mining-lead"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "mining-nickel"
+	},	
+	{
+		type = "unlock-recipe",
+		recipe = "mining-quartz"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "mining-rutile"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "mining-silver"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "mining-stone"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "mining-tin"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "mining-tungsten"
+	},
+	{
+		type = "unlock-recipe",
+		recipe = "mining-zinc"
+	},
+	
+	
+  },
+ 
+  unit =
+  {
+    count = 50,
+    ingredients = {
+      {"science-pack-1", 1},
+      {"science-pack-2", 1},
+	  {"science-pack-3", 1},
+    },
+    time = 35
+  },
+}
+data:extend({technology})
+
