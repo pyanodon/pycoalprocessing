525dba362af3607291dcd5d00963fa1e2fcf7e01
pyanodon <smylodon@gmail.com>
2017-03-15 20:36:48 -0300

    Added the whole chromium chain
    - Iron oxide isnt smelting anymore, dunno why

--- prototypes/recipes/recipes.lua
+++ prototypes/recipes/recipes.lua
@@ -1,6 +1,30 @@
 -------------------------------------------------------------------------------
 --[[Recipes without new item results]]--
 -------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+--Making Chromium
+local making_chromium ={
+    type = "recipe",
+    name = "making_chromium",
+    category = "hpf",
+    enabled = "false",
+    energy_required = 1,
+    ingredients ={
+        {type="fluid", name="carbon-dioxide", amount=10},
+		{type="item", name="chromite_sand", amount=5},
+		{type="item", name="coke", amount=5},
+		{type="item", name="limestone", amount=5},
+    },
+    results={
+        {type="item", name="chromium", amount=1,},
+    },
+    main_product= "chromium",
+    icon = "__pycoalprocessing__/graphics/icons/chromium.png",
+    subgroup = "py-items-hpf",
+    order = "hpf-[chromium]",
+}
+
 --Create coal from wood
 local raw_wood_to_coal = {
     type = "recipe",
@@ -213,10 +237,141 @@ local ref_to_petroleum_gas = {
   main_product= "petroleum-gas",
   order = "c",
 }
+-------------------------------------------------------------------------------
+--COARSE CLASSIFICATION
+local coarse_classification ={
+    type = "recipe",
+    name = "coarse_classification",
+    category = "classifier",
+    enabled = "false",
+    energy_required = 1,
+    ingredients ={
+        {type="item", name="coarse", amount=5},
+    },
+    results={
+        {type="item", name="stone", amount=5},
+		{type="item", name="iron-oxide", amount=2},
+		{type="item", name="gravel", amount=4},
+    },
+    main_product= "gravel",
+    icon = "__pycoalprocessing__/graphics/icons/class-coarse.png",
+    subgroup = "py-items",
+    order = "classifier",
+}
+-------------------------------------------------------------------------------
+--PURE SAND CLASSIFICATION
+local pure_sand_classification ={
+    type = "recipe",
+    name = "pure_sand_classification",
+    category = "classifier",
+    enabled = "false",
+    energy_required = 1,
+    ingredients ={
+        {type="item", name="pure_sand", amount=16},
+    },
+    results={
+        {type="item", name="rich_dust", amount=5},
+		{type="item", name="gravel", amount=2},
+    },
+    main_product= "rich_dust",
+    icon = "__pycoalprocessing__/graphics/icons/class-pure-sand.png",
+    subgroup = "py-items",
+    order = "classifier",
+}
 
+-------------------------------------------------------------------------------
+--SOIL SEPARATION
+local soil_separation ={
+    type = "recipe",
+    name = "soil_separation",
+    category = "solid-separator",
+    enabled = "false",
+    energy_required = 1,
+    ingredients ={
+        {type="item", name="soil", amount=10},
+    },
+    results={
+        {type="item", name="sand", amount=7},
+		{type="item", name="coarse", amount=3},
+		{type="item", name="limestone", amount=4},
+		{type="item", name="organics", amount=2},
+    },
+    main_product= "sand",
+    icon = "__pycoalprocessing__/graphics/icons/soil-separation.png",
+    subgroup = "py-items",
+    order = "sand",
+}
+-------------------------------------------------------------------------------
+--RICH DUST SEPARATION
+local richdust_separation ={
+    type = "recipe",
+    name = "richdust_separation",
+    category = "solid-separator",
+    enabled = "false",
+    energy_required = 1,
+    ingredients ={
+        {type="item", name="rich_dust", amount=10},
+    },
+    results={
+        {type="item", name="iron-oxide", amount=2},
+		{type="item", name="chromite_sand", amount=6},
+
+    },
+    main_product= "chromite_sand",
+    icon = "__pycoalprocessing__/graphics/icons/richdust-separation.png",
+    subgroup = "py-items",
+    order = "richdust",
+}
+
+-------------------------------------------------------------------------------
+--TAILINGS SEPARATION
+local tailings_separation ={
+    type = "recipe",
+    name = "tailings_separation",
+    category = "solid-separator",
+    enabled = "false",
+    energy_required = 1,
+    ingredients ={
+        {type="item", name="tailings-dust", amount=15},
+    },
+    results={
+        {type="item", name="coal_dust", amount=8},
+		{type="item", name="rich_dust", amount=2},
+
+    },
+    main_product= "rich_dust",
+    icon = "__pycoalprocessing__/graphics/icons/tailings_separation.png",
+    subgroup = "py-items",
+    order = "richdust",
+}
+
+
+-------------------------------------------------------------------------------
+--SAND WASHING
+local sand_washing ={
+    type = "recipe",
+    name = "sand_washing",
+    category = "washer",
+    enabled = "false",
+    energy_required = 1,
+    ingredients ={
+        {type="item", name="sand", amount=10},
+		{type="fluid", name="water", amount=20},
+    },
+    results={
+        {type="item", name="pure_sand", amount=8},
+		{type="fluid", name="dirty-water", amount=8},
+
+    },
+    main_product= "pure_sand",
+    icon = "__pycoalprocessing__/graphics/icons/sand-washer.png",
+    subgroup = "py-items",
+    order = "c",
+}
 -------------------------------------------------------------------------------
 data:extend{
   raw_wood_to_coal, wood_to_coal, extract_sulfur, cooling_water, fluegas_to_syngas,
   ref_to_petroleum_gas, ref_to_light_oil, petgas_from_refsyngas, light_oil_from_syngas,
-  heavy_oil_from_coal_gas,
+  heavy_oil_from_coal_gas, soil_separation, richdust_separation, sand_washing, tailings_separation,
+  making_chromium, coarse_classification, pure_sand_classification
 }
