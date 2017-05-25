26b0e29fb8deb4bc034443ae50db51dea85c9b8f
pyanodon <smylodon@gmail.com>
2017-05-25 19:46:50 -0300

    more fixes

--- prototypes/fluids/acidgas.lua
+++ prototypes/fluids/acidgas.lua
@@ -12,6 +12,7 @@ local fluid=
     base_color = {r = 0.741, g = 0.788, b = 0.196},
     flow_color = {r = 0.741, g = 0.788, b = 0.196},
     max_temperature = 100,
+	gas_temperature = 15,
     pressure_to_speed_ratio = 0.4,
     flow_to_energy_ratio = 0.59,
 }
