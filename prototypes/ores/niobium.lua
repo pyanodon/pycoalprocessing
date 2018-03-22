8804222a0b68865903473c9db5eddd651b833332
pyanodon <smylodon@gmail.com>
2018-03-21 22:44:19 -0300

    balance and added niobium and niobium mine
    Former-commit-id: e1d90cf8e5e0a084294ab06dfe3afb11bddb8ec1 [formerly 455dbf61527a500eab0b134bea2a21926e2a5101]
    Former-commit-id: e1d90cf8e5e0a084294ab06dfe3afb11bddb8ec1
    Former-commit-id: c5860cc64086eb277eab6dd2477a7b9149f42fa8

--- prototypes/ores/niobium.lua
+++ prototypes/ores/niobium.lua
@@ -1,6 +1,6 @@
 DATA {
     type = "autoplace-control",
-    name = "borax",
+    name = "niobium",
     richness = true,
     order = "b-e",
     category = "resource"
@@ -8,45 +8,44 @@ DATA {
 
 DATA {
     type = "noise-layer",
-    name = "borax"
+    name = "niobium"
 }
 
 ENTITY {
     type = "resource",
-    name = "borax",
-    category = "borax",
-    icon = "__pycoalprocessing__/graphics/icons/raw-borax.png",
+    name = "niobium",
+    category = "niobium",
+    icon = "__pycoalprocessing__/graphics/icons/niobium-ore.png",
 	icon_size = 32,
     flags = {"placeable-neutral"},
     order = "a-b-a",
-    map_color = {r = 0.917, g = 0.917, b = 0.917},
+    map_color = {r = 0.403, g = 0.6, b = 0.701},
     highlight = true,
-    minimum = 400,
-    normal = 1000,
-    maximum = 4000,
+    minimum = 600,
+    normal = 1200,
+    maximum = 5000,
     minable = {
         hardness = 1.5,
-        -- mining_particle = "borax-particle",
+        -- mining_particle = "niobium-particle",
         mining_time = 3,
         results = {
-            {"raw-borax", 1}
+            {"niobium-ore", 1}
         },
-        fluid_amount = 50,
-        required_fluid = "syngas"
+        fluid_amount = 60,
+        required_fluid = "refsyngas"
     },
     collision_box = {{-0.1, -0.1}, {0.1, 0.1}},
     selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
     autoplace = {
-        control = "borax",
+        control = "niobium",
         sharpness = 1,
-        starting_area = true,
         richness_multiplier = 1500,
         richness_multiplier_distance_bonus = 30,
         richness_base = 500,
-        coverage = 0.012,
+        coverage = 0.01,
         peaks = {
             {
-                noise_layer = "borax",
+                noise_layer = "niobium",
                 noise_octaves_difference = -1.5,
                 noise_persistence = 0.3,
             }
@@ -55,14 +54,14 @@ ENTITY {
     stage_counts = {2000, 1600, 1400, 1200, 800, 400, 200, 10},
     stages = {
         sheet = {
-            filename = "__pycoalprocessing__/graphics/ores/borax.png",
+            filename = "__pycoalprocessing__/graphics/ores/niobium.png",
             priority = "extra-high",
             width = 64,
             height = 64,
             frame_count = 8,
             variation_count = 8,
             hr_version = {
-                filename = "__pycoalprocessing__/graphics/ores/hr-borax.png",
+                filename = "__pycoalprocessing__/graphics/ores/hr-niobium.png",
                 priority = "extra-high",
                 width = 128,
                 height = 128,
