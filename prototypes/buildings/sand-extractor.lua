b9907dc6aa48e4818eb2beb1bd393ab45f06b04a
pyanodon <smylodon@gmail.com>
2018-05-12 20:06:46 -0300

    Balances

--- prototypes/buildings/sand-extractor.lua
+++ prototypes/buildings/sand-extractor.lua
@@ -43,7 +43,7 @@ ENTITY {
     module_specification = {
         module_slots = 4
     },
-    allowed_effects = {"consumption", "productivity", "pollution"},
+    allowed_effects = {"consumption", "productivity", "pollution", "speed"},
     crafting_categories = {"sand-extractor"},
     crafting_speed = 1,
     energy_source = {
