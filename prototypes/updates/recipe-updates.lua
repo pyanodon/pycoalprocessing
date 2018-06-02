a175c763cb50d5a56b5ab2c2a26f1469c3f63d93
Nicholas Dunn <nexela@gmail.com>
2018-06-02 19:36:53 -0400

    Fixes

--- prototypes/updates/recipe-updates.lua
+++ prototypes/updates/recipe-updates.lua
@@ -4,7 +4,7 @@ if PYC.USE_CREOSOTE_RECIPES then -- flag from config.lua
 
     if PYC.USE_CREOSOTE_IN.rail then
         local bi_rail = RECIPE("bi-rail-wood")
-        if not bi_rail:valid() then
+        if not bi_rail:is_valid() then
             RECIPE("rail"):clear_ingredients():add_ingredient({"stone", 1}):add_ingredient({"iron-stick", 2}):add_ingredient({"treated-wood", 2})
         else
             bi_rail:clear_ingredients():add_ingredient({"stone", 1}):add_ingredient({"iron-stick", 2}):add_ingredient({"treated-wood", 2})
