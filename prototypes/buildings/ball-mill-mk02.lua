ac0685548cbe714130fb965e41d2b3ddd4361182
pyanodon <smylodon@gmail.com>
2018-11-19 20:30:00 -0200

    ball-mill
    Former-commit-id: 75f3b898b436bb739f4fd4e128c7f7ff6b118260 [formerly d52c6f94efe28aaa43e6836f3a8564ede12291e6]
    Former-commit-id: 75f3b898b436bb739f4fd4e128c7f7ff6b118260
    Former-commit-id: b08f1b218e80bc9732cc15de4601784815e81e33

--- prototypes/buildings/ball-mill-mk02.lua
+++ prototypes/buildings/ball-mill-mk02.lua
@@ -1,39 +1,39 @@
 RECIPE {
     type = "recipe",
-    name = "ball-mill",
+    name = "ball-mill-mk02",
     energy_required = 35,
     enabled = false,
     ingredients = {
-        {"assembling-machine-2", 1},
-        {"concrete", 50},
-        {"copper-plate", 30},
-        {"steel-plate", 40}
+        {"ball-mill-mk01", 1},
+        {"tin-plate", 30},
+        {"engine-unit", 1},
+        {"advanced-circuit", 5},
     },
     results = {
-        {"ball-mill", 1}
+        {"ball-mill-mk02", 1}
     }
-}:add_unlock("crusher")
+}
 
 ITEM {
     type = "item",
-    name = "ball-mill",
-    icon = "__pycoalprocessing__/graphics/icons/ball-mill.png",
+    name = "ball-mill-mk02",
+    icon = "__pycoalprocessing__/graphics/icons/ball-mill-mk02.png",
     icon_size = 32,
     flags = {"goes-to-quickbar"},
     subgroup = "coal-processing",
     order = "d",
-    place_result = "ball-mill",
+    place_result = "ball-mill-mk02",
     stack_size = 10
 }
 
 ENTITY {
     type = "assembling-machine",
-    name = "ball-mill",
-    icon = "__pycoalprocessing__/graphics/icons/ball-mill.png",
+    name = "ball-mill-mk02",
+    icon = "__pycoalprocessing__/graphics/icons/ball-mill-mk02.png",
     icon_size = 32,
     flags = {"placeable-neutral", "player-creation"},
-    minable = {mining_time = 1, result = "ball-mill"},
-    fast_replaceable_group = "ball-mill",
+    minable = {mining_time = 1, result = "ball-mill-mk02"},
+    fast_replaceable_group = "ball-mill-mk02",
     max_health = 400,
     corpse = "big-remnants",
     dying_explosion = "medium-explosion",
@@ -44,21 +44,21 @@ ENTITY {
     },
     allowed_effects = {"consumption", "speed", "productivity", "pollution"},
     crafting_categories = {"ball-mill"},
-    crafting_speed = 0.65,
+    crafting_speed = 2,
     energy_source = {
         type = "electric",
         usage_priority = "secondary-input",
         emissions = 0.05 / 2.6
     },
-    energy_usage = "200kW",
+    energy_usage = "400kW",
     ingredient_count = 6,
     animation = {
-        filename = "__pycoalprocessing__/graphics/entity/ball-mill/ball-mill.png",
+        filename = "__pycoalprocessing__/graphics/entity/ball-mill/ball-mill-mk02.png",
         width = 215,
         height = 198,
         frame_count = 80,
         line_length = 8,
-        animation_speed = 0.9,
+        animation_speed = 0.6,
         shift = {0.318, -0.12}
     },
     vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.55},
---
+++ prototypes/buildings/ball-mill-mk03.lua
@@ -0,0 +1,70 @@
+RECIPE {
+    type = "recipe",
+    name = "ball-mill-mk03",
+    energy_required = 35,
+    enabled = false,
+    ingredients = {
+        {"ball-mill-mk02", 1},
+        {"niobium-plate", 15},
+        {"electric-engine-unit", 2},
+        {"processing-unit", 10},
+    },
+    results = {
+        {"ball-mill-mk03", 1}
+    }
+}
+
+ITEM {
+    type = "item",
+    name = "ball-mill-mk03",
+    icon = "__pycoalprocessing__/graphics/icons/ball-mill-mk03.png",
+    icon_size = 32,
+    flags = {"goes-to-quickbar"},
+    subgroup = "coal-processing",
+    order = "d",
+    place_result = "ball-mill-mk03",
+    stack_size = 10
+}
+
+ENTITY {
+    type = "assembling-machine",
+    name = "ball-mill-mk03",
+    icon = "__pycoalprocessing__/graphics/icons/ball-mill-mk03.png",
+    icon_size = 32,
+    flags = {"placeable-neutral", "player-creation"},
+    minable = {mining_time = 1, result = "ball-mill-mk03"},
+    fast_replaceable_group = "ball-mill-mk03",
+    max_health = 400,
+    corpse = "big-remnants",
+    dying_explosion = "medium-explosion",
+    collision_box = {{-2.9, -2.9}, {2.9, 2.9}},
+    selection_box = {{-3.0, -3.0}, {3.0, 3.0}},
+    module_specification = {
+        module_slots = 4
+    },
+    allowed_effects = {"consumption", "speed", "productivity", "pollution"},
+    crafting_categories = {"ball-mill"},
+    crafting_speed = 3,
+    energy_source = {
+        type = "electric",
+        usage_priority = "secondary-input",
+        emissions = 0.05 / 2.6
+    },
+    energy_usage = "600kW",
+    ingredient_count = 6,
+    animation = {
+        filename = "__pycoalprocessing__/graphics/entity/ball-mill/ball-mill-mk03.png",
+        width = 215,
+        height = 198,
+        frame_count = 80,
+        line_length = 8,
+        animation_speed = 0.4,
+        shift = {0.318, -0.12}
+    },
+    vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.55},
+    working_sound = {
+        sound = {filename = "__pycoalprocessing__/sounds/niobium-mine.ogg"},
+        idle_sound = {filename = "__pycoalprocessing__/sounds/niobium-mine.ogg", volume = 0.65},
+        apparent_volume = 2.5
+    }
+}
