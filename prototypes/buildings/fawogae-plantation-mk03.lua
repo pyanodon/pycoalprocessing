66bdc252b40c47058f7b59454cb944e20d4be90d
pyanodon <smylodon@gmail.com>
2018-11-25 19:16:58 -0200

    mk04
    Former-commit-id: 82d9be4f89d6d3e2b37361733a9f83948abdeb03 [formerly b7a14a85032d3be76ca0e36f1739a7af4a8958d5]
    Former-commit-id: 82d9be4f89d6d3e2b37361733a9f83948abdeb03
    Former-commit-id: e36fd999a0a23b43221d06f0c8c965f0211764e6

--- prototypes/buildings/fawogae-plantation-mk03.lua
+++ prototypes/buildings/fawogae-plantation-mk03.lua
@@ -15,41 +15,40 @@ local pipes = {
 
 RECIPE {
     type = "recipe",
-    name = "fawogae-plantation",
+    name = "fawogae-plantation-mk03",
     energy_required = 10,
-    enabled = true,
+    enabled = false,
     ingredients = {
-        {"soil", 300},
-        {"pipe", 15},
-        {"electronic-circuit", 10}, --bob basic-circuit-board
-        {"copper-cable", 25},
-        {"iron-plate", 40},
-        {"raw-wood", 150}
+        {"fawogae-plantation-mk02", 1},
+        {"niobium-plate", 15},
+        {"processing-unit", 10},
+        {"treated-wood", 20}, --pyht fiberboard
+        {"niobium-pipe", 20},
     },
     results = {
-        {"fawogae-plantation", 1}
+        {"fawogae-plantation-mk03", 1}
     }
 }
 
 ITEM {
     type = "item",
-    name = "fawogae-plantation",
-    icon = "__pycoalprocessing__/graphics/icons/fawogae-plantation-icon.png",
+    name = "fawogae-plantation-mk03",
+    icon = "__pycoalprocessing__/graphics/icons/fawogae-plantation-icon-mk03.png",
     icon_size = 32,
     flags = {"goes-to-quickbar"},
     subgroup = "py-extraction",
     order = "b",
-    place_result = "fawogae-plantation",
+    place_result = "fawogae-plantation-mk03",
     stack_size = 10
 }
 
 ENTITY {
     type = "assembling-machine",
-    name = "fawogae-plantation",
-    icon = "__pycoalprocessing__/graphics/icons/fawogae-plantation-icon.png",
+    name = "fawogae-plantation-mk03",
+    icon = "__pycoalprocessing__/graphics/icons/fawogae-plantation-icon-mk03.png",
     icon_size = 32,
     flags = {"placeable-neutral", "player-creation"},
-    minable = {mining_time = 1, result = "fawogae-plantation"},
+    minable = {mining_time = 1, result = "fawogae-plantation-mk03"},
     fast_replaceable_group = "fawogae-plantation",
     max_health = 300,
     corpse = "big-remnants",
@@ -57,25 +56,25 @@ ENTITY {
     collision_box = {{-2.8, -2.8}, {2.8, 2.8}},
     selection_box = {{-3.0, -3.0}, {3.0, 3.0}},
     module_specification = {
-        module_slots = 2
+        module_slots = 0
     },
     allowed_effects = {"consumption", "speed", "productivity", "pollution"},
     crafting_categories = {"fawogae"},
-    crafting_speed = 0.3,
+    crafting_speed = 1,
     energy_source = {
         type = "electric",
         usage_priority = "secondary-input",
         emissions = 0.01
     },
-    energy_usage = "200kW",
+    energy_usage = "400kW",
     ingredient_count = 1,
     animation = {
-        filename = "__pycoalprocessing__/graphics/entity/fawogae-plantation/fawogae.png",
+        filename = "__pycoalprocessing__/graphics/entity/fawogae-plantation/fawogae-mk03.png",
         width = 200,
         height = 193,
         frame_count = 100,
         line_length = 10,
-        animation_speed = 3.0,
+        animation_speed = 0.6,
         shift = {0.13, -0.03}
     },
     fluid_boxes = {
