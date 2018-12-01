317ce09feb9231174fe037e8406e9cb499351060
pyanodon <smylodon@gmail.com>
2018-12-01 13:53:56 -0200

    borax-balance

--- prototypes/buildings/borax-mine-mk03.lua
+++ prototypes/buildings/borax-mine-mk03.lua
@@ -58,7 +58,7 @@ ENTITY {
         module_slots = 1
     },
     allowed_effects = {"consumption", "speed", "productivity", "pollution"},
-    mining_speed = 5,
+    mining_speed = 2,
     energy_source = {
         type = "electric",
         usage_priority = "secondary-input",
