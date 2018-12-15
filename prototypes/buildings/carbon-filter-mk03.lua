57751ec9fcadd4eaa51353d17ec798323bcb9624
pyanodon <smylodon@gmail.com>
2018-12-15 08:35:02 -0200

    fixed-niobium
    Former-commit-id: aac3c5f2947274b56ca3852155daeea08f3d328a [formerly 0b0001b6e4bd7ef7f69cc54d961191004109d8b7]
    Former-commit-id: aac3c5f2947274b56ca3852155daeea08f3d328a
    Former-commit-id: 60832f033c576b50a9d64be03038d5349762fa47

--- prototypes/buildings/carbon-filter-mk03.lua
+++ prototypes/buildings/carbon-filter-mk03.lua
@@ -16,14 +16,14 @@ RECIPE {
         {"carbon-filter-mk02", 1},
         {"processing-unit", 5},
         {"niobium-plate", 20},
-        {"niobium-pipe", 10},
+        {"pipe", 10},
         {"electric-engine-unit", 1},
         {"kevlar", 10}
     },
     results = {
         {"carbon-filter-mk03", 1}
     }
-}
+}:replace_ingredient("pipe", "niobium-pipe")
 
 ITEM {
     type = "item",
