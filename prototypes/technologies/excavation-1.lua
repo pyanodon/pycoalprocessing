8648fdf1ef57d42214738be6932e5b4ee9237c02
pyanodon <smylodon@gmail.com>
2019-01-27 10:19:38 -0200

    fixes
    Former-commit-id: d35a0244bb4bad130e19b400938e4d63bc2ce704 [formerly 215f1506ca72d1de3b346764620da1186a0d2a06]
    Former-commit-id: d35a0244bb4bad130e19b400938e4d63bc2ce704
    Former-commit-id: 583080838fdf57abdc467640f0ef2039c5c9ad12

--- prototypes/technologies/excavation-1.lua
+++ prototypes/technologies/excavation-1.lua
@@ -4,8 +4,8 @@ TECHNOLOGY {
     icon = "__pycoalprocessing__/graphics/technology/excavation-1.png",
     icon_size = 128,
     order = "c-a",
-    upgrade = true,
-    prerequisites = {"coal-processing-3"}, --bob titatnium-processing
+    --upgrade = true,
+    prerequisites = {"coal-processing-3"}, 
     effects = {},
     unit = {
         count = 45,
