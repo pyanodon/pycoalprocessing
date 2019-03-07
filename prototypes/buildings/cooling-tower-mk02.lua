c6ee82f305f04d05df863c7bd26ddab56c32651a
pyanodon <smylodon@gmail.com>
2019-03-07 13:09:37 -0300

    fixed- cooling tower

--- prototypes/buildings/cooling-tower-mk02.lua
+++ prototypes/buildings/cooling-tower-mk02.lua
@@ -52,11 +52,11 @@ ENTITY {
     collision_box = {{-2.4, -2.4}, {2.4, 2.4}},
     selection_box = {{-2.5, -2.5}, {2.5, 2.5}},
     module_specification = {
-        module_slots = 1
+        module_slots = 0
     },
     allowed_effects = {"speed"},
     crafting_categories = {"cooling"},
-    crafting_speed = 2,
+    crafting_speed = 2.5,
     energy_source = {
         type = "electric",
         usage_priority = "secondary-input",
