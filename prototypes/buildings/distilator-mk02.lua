1e950aefed2b855a6d57bd3daf38e3901fe00b74
pyanodon <smylodon@gmail.com>
2019-03-24 17:29:31 -0300

    removed-white spaces
    Former-commit-id: 96a388393b2bc99f03a44e9fde954f35b5a1f59e [formerly 84a02e46f6e1d4d54f75f370cfe05ed3af1eaa0f]
    Former-commit-id: 96a388393b2bc99f03a44e9fde954f35b5a1f59e
    Former-commit-id: ee5cd69c369e605848954206b5ceb517f9ed6c5d

--- prototypes/buildings/distilator-mk02.lua
+++ prototypes/buildings/distilator-mk02.lua
@@ -157,7 +157,7 @@ ENTITY {
     },
     vehicle_impact_sound = {filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65},
     working_sound = {
-        sound = {filename = "__pycoalprocessing__/sounds/distilator.ogg", volume = 1.2}, 
+        sound = {filename = "__pycoalprocessing__/sounds/distilator.ogg", volume = 1.2},
         idle_sound = {filename = "__pycoalprocessing__/sounds/distilator.ogg", volume = 0.9},
         apparent_volume = 2.5
     }
