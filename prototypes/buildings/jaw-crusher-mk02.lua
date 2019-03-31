589302514620311f0a733045d614fd31962d7980
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-03-31 01:50:51 -0400

    updated emissions
    updated old emissions to emissions per second per watt before the old way was broken by factorio
    
    
    Former-commit-id: 88702fb08aa2534699c93630bbefcada2d6c95a4 [formerly b473f91f43a5795d380bfc9161e23f8a6ec01d9b]
    Former-commit-id: 88702fb08aa2534699c93630bbefcada2d6c95a4
    Former-commit-id: 6b7cc4942d86b8bc3a92c9789af6bc184a006a18

--- prototypes/buildings/jaw-crusher-mk02.lua
+++ prototypes/buildings/jaw-crusher-mk02.lua
@@ -47,7 +47,7 @@ ENTITY {
     energy_source = {
         type = "electric",
         usage_priority = "secondary-input",
-        emissions = 0.001,
+        emissions_per_second_per_watt = 1e-06,
     },
     energy_usage = "400kW",
     ingredient_count = 20,
--- prototypes/buildings/niobium-mine.lua
+++ prototypes/buildings/niobium-mine.lua
@@ -62,7 +62,7 @@ ENTITY {
         fuel_category = "drill",
         effectivity = 4,
         fuel_inventory_size = 1,
-        emissions = 0.001,
+        emissions_per_second_per_watt = 1e-06,
         smoke = {
             {
                 name = "smoke",
