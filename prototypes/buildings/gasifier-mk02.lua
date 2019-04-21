4a409e41b0d27cb113b0233c39e9b2a095889cb8
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-04-20 22:18:10 -0400

    update pollution values to the easier to use per minute

--- prototypes/buildings/gasifier-mk02.lua
+++ prototypes/buildings/gasifier-mk02.lua
@@ -58,7 +58,7 @@ ENTITY {
     energy_source = {
         type = "electric",
         usage_priority = "secondary-input",
-        emissions_per_second_per_watt = 1e-06,
+        emissions_per_minute = 50,
     },
     energy_usage = "400kW",
     ingredient_count = 20,
