0afeb7f33aab51793b256ef49678c19292522e3e
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-06-07 06:45:22 -0400

    working resource gen

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -3,3 +3,36 @@ if DEBUG then
     local developer = require("__stdlib__/stdlib/data/developer/developer")
     developer.make_test_entities("pycoalprocessing")
 end
+
+for _, e in pairs(data.raw.resource) do
+
+	data.raw.resource[e.name]["autoplace"] = nil
+	data.raw["autoplace-control"][e.name] = nil
+	
+end
+
+data.raw["map-gen-presets"]["default"]["rail-world"].basic_settings =
+{
+    property_expression_names = {},
+	autoplace_controls = {},
+	terrain_segmentation = "very-low",
+	water = "high",
+}
+
+data.raw["map-gen-presets"]["default"]["ribbon-world"].basic_settings =
+{
+	autoplace_controls = {},
+	terrain_segmentation = 4,
+	water = 0.25,
+	starting_area = 3,
+	height = 128
+}
+
+data.raw["map-gen-presets"]["default"]["rich-resources"] =
+{
+	order = "b",
+	basic_settings =
+	{
+		autoplace_controls = {}
+	}
+}
--- scripts/resource-generation.lua
+++ scripts/resource-generation.lua
@@ -1,3 +1,7 @@
+local ore_gen = {}
+
+local Event = require("__stdlib__/stdlib/event/event")
+
 --add the pymods resources that are duplicates of the same resource
 --i.e. the oils and phosphate rocks
 
@@ -5,24 +9,40 @@ local resources = {}
 
 local procks = {}
 
+local rocks = {}
+local rockset = {}
+
 local oilpatches = {}
 
 local sresources =
 	{
-	"iron",
-	"copper",
+	"iron-ore",
+	"copper-ore",
 	"coal",
 	"stone",
 	"borax"
 	}
+
+function ore_gen.on_init()
+	global.ore_gen = {}
+	
 	
 if game.active_mods["pyrawores"] then
 
-table.insert(sresources,"quartz")
-table.insert(sresources,"tin")
+table.insert(sresources,"ore-quartz")
+table.insert(sresources,"ore-tin")
 
+--getting rocks
+	for _, r in pairs(game.entity_prototypes) do
+		if r.type == "resource" then
+			if string.find(r.name, "rock") ~= nil then
+				table.insert(rocks, r.name)
+			end
+		end
+	end
 end
 
+--log(serpent.block(rocks))
 
 if game.active_mods["pyhightech"] and game.active_mods["pyrawores"] then
 
@@ -48,10 +68,15 @@ oilpatches =
 
 table.insert(resources,oilpatches)
 
+table.insert(sresources,"tar-patch")
+table.insert(sresources,"oil-mk01")
+
+table.insert(rocks, "tar-patch")
+table.insert(rocks, "oil-mk01")
+
 end
 
-script.on_init(function(event)
-
+	
 local sores =
 	{
 	"phosphate-rock",
@@ -61,33 +86,42 @@ local sores =
 	"oil-mk03",
 	"oil-mk04"
 	}
-
+--[[
 for _, ep in pairs(game.entity_prototypes) do
 	if ep.type == "resource" then
 		for _, r in pairs(sores) do
 			if ep.name ~= r then
-				table.insert(resources, ep.name)
+				for _, i in pairs(resources) do
+					if ep.name ~= i then
+						table.insert(resources, ep.name)
+					end
+				end
 			end
 		end
 	end
 	
 end
+]]--
+--log(serpent.block(resources))
 
-end)
+	for _, r in pairs(rocks) do
+		rockset[r] = true
+	end
 
-script.on_event(defines.events.on_chunk_generated, function(event)
---find and remove old resources
-local oldores = surface.find_entites_filtered{area = {{event.area.left_top.x,event.area.left_top.y},{event.area.right_bottom.x,event.area.right_bottom.y}}, type = "resource"}
-
-for _, o in pairs(oldores) do
-	o.destroy
 end
+Event.register(Event.core_events.init, ore_gen.on_init)
+
+function ore_gen.on_chunk_generated(event)
+
+--log("generating resources")
+
+if math.random(1,4) == 1 then
 
 --spawn new randomized resources
 
 --starting area needs: iron, copper, coal, tin, quartz
 -- need to fit in oil and tar patches
---outer starting area spawn borax, lead
+--outer starting area spawn borax, lead, aluminium
 
 local tx = event.area.left_top.x
 local ty = event.area.left_top.y
@@ -97,18 +131,64 @@ local by = event.area.right_bottom.y
 local Randx = math.random(tx,bx)
 local Randy = math.random(ty,by)
 
+local patchdiamiter = math.random(10,25)
+local oreamount = math.random(1,8)*1000
+
 if tx >= -200 and tx <= 200 and ty >= -200 and ty <= 200 then
 	--is "starting area"
-	if tx >= -150 and tx <= 150 then
+	if tx >= -100 and tx <= 100 and ty >= -100 and ty <= 100 then
 		--inner start area for needed ores
 		--get a random ore to spawn
-		local tsize
-		for _,ts in pairs(sresources) do
-			tsize = tsize+1
+		local tsize = 0
+		if tsize == 0 then
+			for _,ts in pairs(sresources) do
+				tsize = tsize+1
+			end
 		end
-		local randore = sresources[math.random(1,tsize)
-		game.surface["nauvis"].create_entity{name=randore,position={Randx,Randy},amount=123456789}
+		log(tsize)
+		log(serpent.block(sresources))
+		if tsize > 0 then
+			local sindex = math.random(1,tsize)
+			local randore = sresources[sindex]
+			tsize=tsize-1
+			table.remove(sresources,sindex)
+			if string.find(game.surfaces["nauvis"].get_tile(Randx,Randy).name, "water") == nil then
+				if rockset[randore] then
+					oreamount=oreamount*1000
+					game.surfaces["nauvis"].create_entity{name=randore,position={Randx,Randy},amount=oreamount}
+				else
+					game.surfaces["nauvis"].create_entity{name=randore,position={Randx,Randy},amount=oreamount}
+				end
+			end
+				if rockset[randore] == nil then
+				--create patches for ores
+				local patchx = Randx - 10
+				local patchy = Randy - 10
+				local center = {Randx,Randy}
+					for i = 0,400 do
+						--log(patchx..","..patchy)
+						local entity = game.surfaces["nauvis"].find_entities({{patchx,patchy},{patchx,patchy}})
+							--log("patchx "..patchx)
+							--log("randx "..Randx)
+							--log(math.abs(patchx-Randx))
+							--log(patchdiamiter)
+								if math.abs(patchx - Randx) + math.abs(patchy - Randy) <= patchdiamiter/2 or math.random(1,2) == 1 then
+									if string.find(game.surfaces["nauvis"].get_tile(patchx,patchy).name, "water") == nil then
+										game.surfaces["nauvis"].create_entity{name=randore,position={patchx,patchy},amount=oreamount}
+									end
+								end
+						patchx=patchx+1
+							if patchx == Randx+10 then
+								patchx = Randx-10
+								patchy=patchy+1
+							end
+					end
+				end
+		end
 	end
 end
 
-end)
+end
+
+end
+Event.register(defines.events.on_chunk_generated, ore_gen.on_chunk_generated)
