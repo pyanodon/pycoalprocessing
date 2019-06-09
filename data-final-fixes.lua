712c80ef2a455ebe582ff40fd2dcd613065a1f20
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-06-09 08:43:20 -0400

    fixed resource gen to handle old save with new system

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -4,6 +4,7 @@ if DEBUG then
     developer.make_test_entities("pycoalprocessing")
 end
 
+if not mods["rso-mod"] then
 if mods["pyrawores"] or mods["pypetroleumhandling"] then
 for _, e in pairs(data.raw.resource) do
 	data.raw.resource[e.name]["autoplace"] = nil
@@ -33,4 +34,5 @@ data.raw["map-gen-presets"]["default"]["rich-resources"] =
 		autoplace_controls = {}
 	}
 }
+end
 end
