b9e0b3d05a63a3745151cb0fb2185006290fc61b
Nexela <nexela@gmail.com>
2019-06-28 22:51:54 -0400

    Fix duplication and horrendous formating
    Former-commit-id: 9d060e85cb74486d08bffcfe0e30eb58130c98a7 [formerly 2c51b64ee8dca8d4f7367e1b2201ddd081e74b31]
    Former-commit-id: 9d060e85cb74486d08bffcfe0e30eb58130c98a7
    Former-commit-id: 9d3ef0eed846a8a4a2ac973e28ed4568996e233f

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,40 +1,36 @@
-local DEBUG = require("config").DEBUG
+local DEBUG = require('config').DEBUG
 if DEBUG then
-    local developer = require("__stdlib__/stdlib/data/developer/developer")
-    developer.make_test_entities("pycoalprocessing")
+    local developer = require('__stdlib__/stdlib/data/developer/developer')
+    developer.make_test_entities('pycoalprocessing')
 end
 
-if settings.startup["ore-gen"].value then
-if not mods["rso-mod"] and not mods["PyCoalTBaA"] and not mods["PyBlock"] then
-if mods["pyrawores"] or mods["pypetroleumhandling"] then
-for _, e in pairs(data.raw.resource) do
-	data.raw.resource[e.name]["autoplace"] = nil
-	data.raw["autoplace-control"][e.name] = nil
-end
+if settings.startup['ore-gen'].value then
+    if not mods['rso-mod'] and not mods['PyCoalTBaA'] and not mods['PyBlock'] then
+        if mods['pyrawores'] or mods['pypetroleumhandling'] then
+            for _, e in pairs(data.raw.resource) do
+                data.raw.resource[e.name]['autoplace'] = nil
+                data.raw['autoplace-control'][e.name] = nil
+            end
 
-data.raw["map-gen-presets"]["default"]["rail-world"].basic_settings =
-{
-    property_expression_names = {},
-	autoplace_controls = {},
-	terrain_segmentation = "very-low",
-	water = "high",
-}
-data.raw["map-gen-presets"]["default"]["ribbon-world"].basic_settings =
-{
-	autoplace_controls = {},
-	terrain_segmentation = 4,
-	water = 0.25,
-	starting_area = 3,
-	height = 128
-}
-data.raw["map-gen-presets"]["default"]["rich-resources"] =
-{
-	order = "b",
-	basic_settings =
-	{
-		autoplace_controls = {}
-	}
-}
-end
-end
+            data.raw['map-gen-presets']['default']['rail-world'].basic_settings = {
+                property_expression_names = {},
+                autoplace_controls = {},
+                terrain_segmentation = 'very-low',
+                water = 'high'
+            }
+            data.raw['map-gen-presets']['default']['ribbon-world'].basic_settings = {
+                autoplace_controls = {},
+                terrain_segmentation = 4,
+                water = 0.25,
+                starting_area = 3,
+                height = 128
+            }
+            data.raw['map-gen-presets']['default']['rich-resources'] = {
+                order = 'b',
+                basic_settings = {
+                    autoplace_controls = {}
+                }
+            }
+        end
+    end
 end
