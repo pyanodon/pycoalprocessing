c002e1ac15768e4a1277e2db5d04ad83b96e98ff
kingarthur91 <arthurkoy_durbin@yahoo.com>
2019-07-02 19:47:52 -0400

    Tailings pond changes (#65)
    * testing changes to tailings pond spill effects
    
    * operation burn it all is a go go
    
    * added burnt ground
    
    * added filter to only change burnt pooluted ground
    
    * formatted file
    
    * removed preset table from local stile
    
    * reset scorch tich/chance to defualt value
    
    
    Former-commit-id: e9dd6166ed193d1814d379cdda0216aa0ff3cb49 [formerly 3457e2f273e77a7f2c16e47740b2d76325d2e3e8]
    Former-commit-id: e9dd6166ed193d1814d379cdda0216aa0ff3cb49
    Former-commit-id: 27645cbc9b5d32580f80260d8b830de24f8b43c3

--- scripts/tailings-pond.lua
+++ scripts/tailings-pond.lua
@@ -10,6 +10,8 @@ local _scorch_chance = CFG.SCORCH_CHANCE
 local _scorch_ticks = CFG.SCORCH_TICKS
 local _pollution_mod = CFG.GAS_POLLUTE_MODIFIER
 
+local Tiles = {}
+
 --Pond contains gases, lets spill them out. Only negative is this can be used as a "gas" void so...
 --If the gas is "polluting" create pollution, else just vent.
 local function empty_pond_gas(fluid, surface, position)
@@ -52,6 +54,8 @@ local function scorch_earth(pond, tick)
     --Vent Gasses
     local fluid = empty_pond_gas(fluidbox[1], pond.entity.surface, pond.entity.position)
 
+    local tiles = {}
+
     --No gasses left if we still have fluid
     if fluid then
         --if full set the full tick or check and spill
@@ -61,17 +65,24 @@ local function scorch_earth(pond, tick)
                 pond.fluid_per = 1
             elseif ((tick >= (pond.full + _scorch_ticks)) and (_scorch_chance > 0 and math.random(1, 100) <= _scorch_chance)) then
                 --spill fluid out here, if not water pollute.
+                --removing emptying code for spillage per pyanodon request
                 fluid.amount = fluid.amount / 2
                 pond.fluid_per = .5
                 pond.full = nil
                 --polluted ground is very difficult to walk on, it also ruins any path tiles near it.
                 --TODO Issues when polluting near water.
                 if fluid.name:contains('dirty') or not fluid.name:contains('water') then
-                    local tiles = {}
-                    for x, y in Area.spiral_iterate(Position.expand_to_area(pond.entity.position, 6)) do
-                        tiles[#tiles + 1] = {name = 'polluted-ground', position = {x = x, y = y}}
+                    local area_radius = 0
+                    repeat
+                        area_radius = area_radius + 6
+                    until (pond.entity.surface.get_tile(pond.entity.position.x + (area_radius), pond.entity.position.y).name ~= 'polluted-ground')
+                    for x, y in Area.spiral_iterate(Position.expand_to_area(pond.entity.position, area_radius)) do
+                        if pond.entity.surface.get_tile(x, y).name ~= 'polluted-ground' then
+                            tiles[#tiles + 1] = {name = 'polluted-ground', position = {x = x, y = y}}
+                        end
                     end
-                    pond.entity.surface.set_tiles(tiles, true)
+                --moving set tiles to a seperate function to spread tile change load over ticks.
+                --needs stdlib adjusts to use std tick spreader. proof of concept at the moment
                 end -- polluting liquids
             end -- Full Pond
         else -- not full fluid
@@ -82,6 +93,7 @@ local function scorch_earth(pond, tick)
     end
     --push the updated fluidbox to the entity.
     fluidbox[1] = fluid
+    return tiles
 end
 
 --Sets animation frame based on tank filled percentage
@@ -112,20 +124,61 @@ function tailings_pond.create(event)
 end
 Event.register(Event.build_events, tailings_pond.create)
 
+local function tile_setter(event)
+    local tiles = event
+    local stiles = {}
+    --log(table_size(tiles))
+    if table_size(tiles) > 400 then
+        for i = 1, 400 do
+            table.insert(stiles, tiles[i])
+            tiles[i] = nil
+        end
+    else
+        stiles = tiles
+        tiles = {}
+    end
+    local temptable = {}
+    for i = 1, table_size(tiles) do
+        if tiles[i] ~= nil then
+            table.insert(temptable, tiles[i])
+        end
+    end
+    tiles = temptable
+    return stiles, tiles
+end
+
 --Run tick handler every 30 ticks. In the future this will need to be spread out to itereate over a queing system.
 function tailings_pond.on_tick(event)
+    --log(table_size(Tiles))
     local ponds = global.tailings_ponds
     for i, pond in pairs(ponds) do
         if pond.entity.valid then
             if type(pond.sprite) == 'number' then
-                scorch_earth(pond, event.tick)
+                local t = scorch_earth(pond, event.tick)
                 --Set the animation needed based on fill level..
                 set_fluid_level_image(pond)
+                if t[1] ~= nil then
+                    Tiles = t
+                end
             else
                 pond.sprite = create_sprite(pond.entity)
             end
         else
             global.tailings_ponds[i] = nil
+        end
+        --log(serpent.block(Tiles))
+        --log(serpent.block(Tiles[1]))
+        if Tiles[1] ~= nil then
+            --log(serpent.block(table_size(Tiles)))
+            local stiles
+            stiles, Tiles = tile_setter(Tiles)
+            --log(serpent.block(table_size(stiles)))
+            --log(serpent.block(table_size(Tiles)))
+            --log(serpent.block(pond))
+            pond.entity.surface.set_tiles(stiles, true)
+            for _, st in pairs(stiles) do
+                pond.entity.surface.create_entity {name = 'ninja-tree', position = {st.position.x, st.position.y}}
+            end
         end
     end
 end
@@ -135,5 +188,14 @@ function tailings_pond.on_init()
     global.tailings_ponds = {}
 end
 Event.register(Event.core_events.init, tailings_pond.on_init)
+
+function tailings_pond.on_entity_died(event)
+    if event.entity.name == 'ninja-tree' then
+        event.entity.surface.set_tiles {{name = 'polluted-ground-burnt', position = event.entity.position}}
+        event.entity.destroy()
+    end
+end
+
+Event.register(defines.events.on_entity_died, tailings_pond.on_entity_died)
 
 return tailings_pond
