0528c2cb3aa86ab72ddcdd800f8189f76a16a6f9
pyanodon <smylodon@gmail.com>
2019-07-10 10:59:51 -0300

    speed up cooling towers.

--- prototypes/buildings/cooling-tower-mk01.lua
+++ prototypes/buildings/cooling-tower-mk01.lua
@@ -45,7 +45,7 @@ ENTITY {
     },
     allowed_effects = {"speed"},
     crafting_categories = {"cooling"},
-    crafting_speed = 1.5,
+    crafting_speed = 2,
     energy_source = {
         type = "electric",
         usage_priority = "secondary-input",
