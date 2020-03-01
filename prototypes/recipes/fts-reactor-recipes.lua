6d2746c5bf36de676e22d9f9da996809f6070b5c
Bobblybook <bobblybook@gmail.com>
2020-03-02 04:38:29 +1100

    Revert combustion/MK changes
    - Revert dynamic combustion mixture localisation
    - Revert MK numbering scheme
    
    
    Former-commit-id: cc3c6d3ec890c7736315147bd6e1772b28113c50 [formerly b9e163ddf3c22922d2b80821c98aadc96a69ba5b]
    Former-commit-id: cc3c6d3ec890c7736315147bd6e1772b28113c50
    Former-commit-id: a7a20a5a5d817ea954853c07332b29b99325f7c8

--- prototypes/recipes/fts-reactor-recipes.lua
+++ prototypes/recipes/fts-reactor-recipes.lua
@@ -115,7 +115,6 @@ RECIPE {
 RECIPE {
     type = "recipe",
     name = "combustion-olefin",
-    localised_name = {"recipe-name.combustion-mixture", {"fluid-name.olefin"}, 700},
     category = "fts-reactor",
     enabled = false,
     energy_required = 9.5,
