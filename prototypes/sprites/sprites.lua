43815f644abf89202810f8864f9927e0b2a87577
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-04-11 06:10:16 -0400

    fixed errors add wiki sprite
    Former-commit-id: 8732e45b4597cfeddae80084dd7d39cde851cfba [formerly de69ddd3d2ff088f5ea93dacb9f329516b78917c]
    Former-commit-id: 8732e45b4597cfeddae80084dd7d39cde851cfba
    Former-commit-id: 5d71cf155f60990e59a883baffa09766557487b3

---
+++ prototypes/sprites/sprites.lua
@@ -0,0 +1,15 @@
+data:extend(
+{
+  {
+    type = "sprite",
+    name = "pywiki",
+    filename = "__pycoalprocessinggraphics__/graphics/py-wiki.png",
+    --priority = "extra-high-no-scale",
+    width = 128,
+    height = 128,
+    --flags = {"gui-icon"},
+    --mipmap_count = 1,
+    --scale = 0.5
+  }
+}
+)
