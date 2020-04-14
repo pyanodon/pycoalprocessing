f970e8a8dffd62ed1f3116d187c46d4be95fcb68
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-04-14 06:58:52 -0400

    wiki
    Former-commit-id: 9e3f41fd55eb3fba60b5baa97efaa2a302cc4928 [formerly a8c8cd0e83d30bb6a2210e91f5c4139cb8f98127]
    Former-commit-id: 9e3f41fd55eb3fba60b5baa97efaa2a302cc4928
    Former-commit-id: 0193f335afb4e3bd9c22b835b8a7dd2e18a1ab59

---
+++ scripts/wiki.lua
@@ -0,0 +1,101 @@
+local Event = require('__stdlib__/stdlib/event/event')
+
+local function on_init()
+    global.wiki = {}
+    global.wiki.fluids = {}
+    for _, fluid in pairs(game.fluid_prototypes) do
+        log(serpent.block(fluid.name))
+        log(serpent.block(fluid.fuel_value))
+        if fluid.fuel_value ~= nil and fluid.fuel_value ~= 0 then
+        global.wiki.fluids[fluid.name] = fluid.fuel_value
+        end
+    end
+end
+Event.register(Event.core_events.init, on_init)
+
+local function on_player_created(event)
+
+    local player = game.players[event.player_index]
+    local wiki = player.gui.top.add(
+        {
+            type = 'frame',
+            name = 'pywiki_frame',
+        }
+    )
+    wiki.add(
+        {
+            type = 'sprite-button',
+            name = 'pywiki',
+            sprite = 'pywiki'
+        }
+    )
+    
+end
+Event.register(defines.events.on_player_created, on_player_created)
+
+local function on_click(event)
+
+    local player = game.players[event.player_index]
+    
+    if event.element.name == 'pywiki' then
+        local wiki_gui = event.element.parent
+        wiki_gui.clear()
+        wiki_gui.add(
+            {
+                type = 'frame',
+                name = 'fluid_page',
+                caption = 'Fluids with fuel value',
+                direction = "vertical"
+            }
+        )
+        wiki_gui.add(
+            {
+                type = "sprite-button",
+                name = "wiki_close",
+                sprite = "utility/close_fat"
+            }
+        )
+        for f, fluid in pairs(global.wiki.fluids) do
+            wiki_gui.fluid_page.add(
+                {
+                    type = 'frame',
+                    name = 'fluid_frame'.. f,
+                    caption = game.fluid_prototypes[f].localised_name,
+                    direction = "horizontal"
+                }
+            )
+            wiki_gui.fluid_page['fluid_frame'.. f].add(
+            {
+                type = 'sprite',
+                name = f,
+                sprite = 'fluid/' .. f,
+                caption = game.fluid_prototypes[f].localised_name 
+            }
+            )
+            local num
+            if fluid >= 1000 then
+                num = fluid/1000 .. 'KJ'
+            elseif fluid >= 100000 then
+                num = fluid/1000000 .. 'MJ'
+            end
+            wiki_gui.fluid_page['fluid_frame'.. f].add(
+            {
+                type = 'frame',
+                name = f .. 'fluid_value',
+                caption = num
+            }
+            )
+        end
+    elseif event.element.name == 'wiki_close' then
+        local wiki_gui = event.element.parent
+        wiki_gui.clear()
+        wiki_gui.add(
+            {
+                type = 'sprite-button',
+                name = 'pywiki',
+                sprite = 'pywiki'
+            }
+        )
+    end
+end
+Event.register(defines.events.on_gui_click, on_click)
