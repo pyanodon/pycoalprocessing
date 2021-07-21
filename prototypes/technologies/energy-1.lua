2be79485656cf87bfdc6d0b9d8c41f867f1fa8b0
ShadowGlass0 <gkecskem@gmail.com>
2021-07-21 18:44:27 +1000

    Update energy-1.lua
    #92 Fix Energy generation 1 prerequisites

--- prototypes/technologies/energy-1.lua
+++ prototypes/technologies/energy-1.lua
@@ -5,7 +5,7 @@ TECHNOLOGY {
     icon_size = 128,
     order = "c-a",
     --upgrade = true,
-    prerequisites = {"coal-processing-1", "steel-processing"},
+    prerequisites = {"coal-processing-1"},
     effects = {},
     unit = {
         count = 30,
