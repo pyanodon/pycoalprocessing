2a8e803dbc5e438238bd2b0fbc373a559dd0bd9f
ShadowGlass0 <gkecskem@gmail.com>
2021-07-24 22:23:59 +1000

    Update niobium.lua
    #131 Fix Niobium prerequisites

--- prototypes/technologies/niobium.lua
+++ prototypes/technologies/niobium.lua
@@ -4,7 +4,7 @@ TECHNOLOGY {
     icon = "__pycoalprocessinggraphics__/graphics/technology/niobium.png",
     icon_size = 128,
     order = "c-a",
-    prerequisites = {"crusher", "separation", "filtration", "chromium","coal-processing-2"},
+    prerequisites = {"crusher", "filtration"},
     effects = {},
     unit = {
         count = 200,
