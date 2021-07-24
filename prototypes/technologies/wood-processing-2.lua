bdad832938ae8e3c9cfe0af36240da651782ed19
ShadowGlass0 <gkecskem@gmail.com>
2021-07-24 22:31:00 +1000

    Update wood-processing-2.lua
    #134 Fix wood processing 2 prerequisites

--- prototypes/technologies/wood-processing-2.lua
+++ prototypes/technologies/wood-processing-2.lua
@@ -5,7 +5,7 @@ TECHNOLOGY {
     icon_size = 128,
     order = "c-a",
     --upgrade = true,
-    prerequisites = {"wood-processing"},
+    prerequisites = {"wood-processing", "logistic-science-pack"},
     effects = {},
     unit = {
         count = 30,
