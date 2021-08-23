b6282d44ba808ddc76d92dbfc2330c850403b9cb
kingarthur91 <arthurkoy_durbin@yahoo.com>
2021-08-23 18:59:48 -0400

    Update fine-electronics.lua
    #175

--- prototypes/technologies/fine-electronics.lua
+++ prototypes/technologies/fine-electronics.lua
@@ -4,7 +4,7 @@ TECHNOLOGY {
     icon = "__pycoalprocessinggraphics__/graphics/technology/fine-electronics.png",
     icon_size = 128,
     order = "c-a",
-    prerequisites = {"niobium","sulfur-processing"},
+    prerequisites = {"niobium","sulfur-processing", "plastics"},
     effects = {},
     unit = {
         count = 30,
