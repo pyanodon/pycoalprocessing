c341a36518f8e205657274d35b80cf5458fee18f
kingarthur91 <arthurkoy_durbin@yahoo.com>
2021-10-06 05:30:12 -0400

    power tweaks

--- prototypes/updates/entity-updates.lua
+++ prototypes/updates/entity-updates.lua
@@ -15,6 +15,20 @@ data.raw.boiler.boiler.target_temperature = 250
 
 --modify reactor to produce more energy
 data.raw.reactor["nuclear-reactor"].consumption = "2GW"
+data.raw.reactor["nuclear-reactor"].heat_buffer.max_temperature = "2000"
+data.raw.reactor["nuclear-reactor"].heat_buffer.specific_heat = "40MJ"
+
+--modify heat exchanger
+data.raw.boiler['heat-exchanger'].target_temperature = 2000
+data.raw.boiler['heat-exchanger'].energy_consumption = "4.25GW"
+data.raw.boiler['heat-exchanger'].energy_source.max_temperature = 2000
+data.raw.boiler['heat-exchanger'].energy_source.specific_heat = "10MJ"
+data.raw.boiler['heat-exchanger'].energy_source.max_transfer = "4GW"
+
+--modify heat pipe
+data.raw['heat-pipe']['heat-pipe'].heat_buffer.max_temperature = 2000
+data.raw['heat-pipe']['heat-pipe'].heat_buffer.max_transfer = "5GW"
+data.raw['heat-pipe']['heat-pipe'].heat_buffer.specific_heat = "10MJ"
 
 data.raw.item["uranium-fuel-cell"].fuel_value = "4GJ"
 
@@ -56,125 +70,3 @@ data.raw.furnace["electric-furnace"].energy_usage = "1MW"
 data.raw.furnace["electric-furnace"].crafting_speed = 4
 data.raw.furnace["electric-furnace"].allowed_effects = nil
 data.raw.furnace["electric-furnace"].module_specification = nil
-
-data.raw.boiler['heat-exchanger'].target_temperature = 2000
-data.raw.boiler['heat-exchanger'].energy_consumption = "4.25GW"
-data.raw.boiler['heat-exchanger'].energy_source =
-    {
-      type = "heat",
-      max_temperature = 2000,
-      specific_heat = "1MJ",
-      max_transfer = "2GW",
-      min_working_temperature = 500,
-      minimum_glow_temperature = 350,
-      connections =
-      {
-        {
-          position = {0, 0.5},
-          direction = defines.direction.south
-        }
-      },
-      pipe_covers =
-        make_4way_animation_from_spritesheet(
-        {
-          filename = "__base__/graphics/entity/heat-exchanger/heatex-endings.png",
-          width = 32,
-          height = 32,
-          direction_count = 4,
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/heat-exchanger/hr-heatex-endings.png",
-            width = 64,
-            height = 64,
-            direction_count = 4,
-            scale = 0.5
-          }
-        }),
-      heat_pipe_covers =
-        make_4way_animation_from_spritesheet(
-        apply_heat_pipe_glow{
-          filename = "__base__/graphics/entity/heat-exchanger/heatex-endings-heated.png",
-          width = 32,
-          height = 32,
-          direction_count = 4,
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/heat-exchanger/hr-heatex-endings-heated.png",
-            width = 64,
-            height = 64,
-            direction_count = 4,
-            scale = 0.5
-          }
-        }),
-      heat_picture =
-      {
-        north = apply_heat_pipe_glow
-        {
-          filename = "__base__/graphics/entity/heat-exchanger/heatex-N-heated.png",
-          priority = "extra-high",
-          width = 24,
-          height = 48,
-          shift = util.by_pixel(-1, 8),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/heat-exchanger/hr-heatex-N-heated.png",
-            priority = "extra-high",
-            width = 44,
-            height = 96,
-            shift = util.by_pixel(-0.5, 8.5),
-            scale = 0.5
-          }
-        },
-        east = apply_heat_pipe_glow
-        {
-          filename = "__base__/graphics/entity/heat-exchanger/heatex-E-heated.png",
-          priority = "extra-high",
-          width = 40,
-          height = 40,
-          shift = util.by_pixel(-21, -13),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/heat-exchanger/hr-heatex-E-heated.png",
-            priority = "extra-high",
-            width = 80,
-            height = 80,
-            shift = util.by_pixel(-21, -13),
-            scale = 0.5
-          }
-        },
-        south = apply_heat_pipe_glow
-        {
-          filename = "__base__/graphics/entity/heat-exchanger/heatex-S-heated.png",
-          priority = "extra-high",
-          width = 16,
-          height = 20,
-          shift = util.by_pixel(-1, -30),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/heat-exchanger/hr-heatex-S-heated.png",
-            priority = "extra-high",
-            width = 28,
-            height = 40,
-            shift = util.by_pixel(-1, -30),
-            scale = 0.5
-          }
-        },
-        west = apply_heat_pipe_glow
-        {
-          filename = "__base__/graphics/entity/heat-exchanger/heatex-W-heated.png",
-          priority = "extra-high",
-          width = 32,
-          height = 40,
-          shift = util.by_pixel(23, -13),
-          hr_version =
-          {
-            filename = "__base__/graphics/entity/heat-exchanger/hr-heatex-W-heated.png",
-            priority = "extra-high",
-            width = 64,
-            height = 76,
-            shift = util.by_pixel(23, -13),
-            scale = 0.5
-          }
-        }
-      }
-    }
