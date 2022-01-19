a635f2c42080c48834c82f2b70e0fde4449791dd
ShadowGlass0 <gkecskem@gmail.com>
2022-01-19 19:44:07 +1100

    boiler energy

--- prototypes/updates/entity-updates.lua
+++ prototypes/updates/entity-updates.lua
@@ -4,7 +4,7 @@ data.raw.generator['steam-engine'].maximum_temperature = 500
 
 --modify boiler to produce more water
 data.raw.boiler.boiler.target_temperature = 250
-data.raw.boiler.boiler.energy_consumption = "14.85MW"
+data.raw.boiler.boiler.energy_consumption = "14.805MW"
 --modify boiler to have burnt result inventory
 data.raw.boiler.boiler.energy_source.burnt_inventory_size = 1
 
