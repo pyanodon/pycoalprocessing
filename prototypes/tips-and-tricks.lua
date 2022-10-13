e3e8c30041978bec00f63b175133d73ae720a5bb
Alex ten Brink <alextenbrink@gmail.com>
2022-10-13 21:58:39 +0200

    Reorganized tips to be more focused.
    Still lots of things on the todo list:
    - expand some of the tips, particularly the 'themes'
      and 'general tips' kind of sections
    - add tips for remaining relevant PyCP techs
    - add tips for remaining modpacks if enabled
    - use recipes instead of items in some places
    - make use of "Pyanodon", "Pyanodon's" etc uniform
    - add icons to tips?
    - add simulations?

--- prototypes/tips-and-tricks.lua
+++ prototypes/tips-and-tricks.lua
@@ -13,69 +13,131 @@ data:extend(
 		starting_status = "suggested",
 		is_title = true,
 	},
+    
 	{
 		type = "tips-and-tricks-item",
-		name = "victory",
+		name = "before-first-game",
 		category = "wiki-py",
 		indent = 1,
 		order = "b",
-		dependencies = {"intro"}
-	},
-	{
-		type = "tips-and-tricks-item",
-		name = "themes-tips",
-		category = "wiki-py",
-		indent = 1,
-		order = "c",
-		dependencies = {"intro"}
-	},
-	{
-		type = "tips-and-tricks-item",
-		name = "mechanical-inserter",
-		category = "wiki-py",
-		indent = 1,
-		order = "d",
-		dependencies = {"intro"}
-	},
-	{
-		type = "tips-and-tricks-item",
-		name = "ash",
-		category = "wiki-py",
-		indent = 1,
-		order = "e",
-		dependencies = {"intro"}
-	},
-	{
-		type = "tips-and-tricks-item",
-		name = "tailings-pond",
-		category = "wiki-py",
-		indent = 1,
-		order = "f",
-		dependencies = {"intro"}
-	},
-	{
-		type = "tips-and-tricks-item",
-		name = "early-game-tips",
-		category = "wiki-py",
-		indent = 1,
-		order = "g",
-		dependencies = {"intro"}
-	},
-	{
-		type = "tips-and-tricks-item",
-		name = "techs",
-		category = "wiki-py",
-		order = "i",
 		starting_status = "suggested",
 		is_title = true,
 	},
 	{
 		type = "tips-and-tricks-item",
-		name = "combustion-mixture",
+		name = "recommended-settings",
+		category = "wiki-py",
+		indent = 2,
+		order = "ba",
+		starting_status = "suggested",
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "recommended-mod-combinations",
+		category = "wiki-py",
+		indent = 2,
+		order = "bb",
+		starting_status = "suggested",
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "recommended-other-mods",
+		category = "wiki-py",
+		indent = 2,
+		order = "bb",
+		starting_status = "suggested",
+	},
+    
+	{
+		type = "tips-and-tricks-item",
+		name = "general-py-tips",
 		category = "wiki-py",
 		indent = 1,
-		order = "j",
-		dependencies = {"techs"},
+		order = "c",
+		starting_status = "suggested",
+		is_title = true,
+	},
+    {
+		type = "tips-and-tricks-item",
+		name = "victory",
+		category = "wiki-py",
+		indent = 2,
+		order = "ca",
+		starting_status = "suggested",
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "themes",
+		category = "wiki-py",
+		indent = 2,
+		order = "cb",
+		starting_status = "suggested",
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "general-tips",
+		category = "wiki-py",
+		indent = 2,
+		order = "cc",
+		starting_status = "suggested",
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "py-technologies",
+		category = "wiki-py",
+		indent = 2,
+		order = "cd",
+		starting_status = "suggested",
+	},
+    
+	{
+		type = "tips-and-tricks-item",
+		name = "coalprocessing",
+		category = "wiki-py",
+		indent = 1,
+		order = "d",
+		starting_status = "suggested",
+		is_title = true,
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "mechanical-inserter",
+		category = "wiki-py",
+		indent = 2,
+		order = "da",
+		dependencies = {"coalprocessing"}
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "ash",
+		category = "wiki-py",
+		indent = 2,
+		order = "db",
+		dependencies = {"coalprocessing"}
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "tailings-pond",
+		category = "wiki-py",
+		indent = 2,
+		order = "dc",
+		dependencies = {"coalprocessing"}
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "early-game-tips",
+		category = "wiki-py",
+		indent = 2,
+		order = "dd",
+		dependencies = {"coalprocessing"}
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "combustion-mixture",
+		category = "wiki-py",
+		indent = 2,
+		order = "de",
+		dependencies = {"coalprocessing"},
 		trigger =
 		{
 			type = "research",
@@ -83,14 +145,16 @@ data:extend(
 		},
 	}
 })
-data:extend(
-{
-	{
-		type = "tips-and-tricks-item",
-		name = "your-first-science-pack",
-		category = "wiki-py",
-		indent = 1,
-		order = "h",
-		dependencies = {"intro"}
-	}
-})
+if not mods["pyalienlife"] then
+    data:extend(
+    {
+        {
+            type = "tips-and-tricks-item",
+            name = "your-first-science-pack",
+            category = "wiki-py",
+            indent = 2,
+            order = "df",
+            dependencies = {"coalprocessing"},
+        }
+    })
+end
