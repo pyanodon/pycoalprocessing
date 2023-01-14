b8f0c5ccfda38710e80f45a274aaa9c790e713aa
Alex ten Brink <alextenbrink@gmail.com>
2023-01-14 11:02:50 +0100

    Clean up and finish PYcp tips
    Remove all the handholdy stuff, and stick to mechanics/facts that are different from the base game.

--- prototypes/tips-and-tricks.lua
+++ prototypes/tips-and-tricks.lua
@@ -1,3 +1,11 @@
+--[[
+order letters:
+pycp: d
+pyro: e
+pyph: f
+pyal: g
+pyae: h
+--]]
 data:extend(
 {
 	{
@@ -9,168 +17,73 @@ data:extend(
 		type = "tips-and-tricks-item",
 		name = "intro",
 		category = "wiki-py",
-		order = "a",
-		starting_status = "suggested",
-		is_title = true,
-	},
-    --[[
-	{
-		type = "tips-and-tricks-item",
-		name = "before-first-game",
-		category = "wiki-py",
-		indent = 1,
 		order = "b",
-		starting_status = "suggested",
 		is_title = true,
 	},
-	{
-		type = "tips-and-tricks-item",
-		name = "recommended-settings",
-		category = "wiki-py",
-		indent = 2,
-		order = "ba",
-		starting_status = "suggested",
-	},
-	{
-		type = "tips-and-tricks-item",
-		name = "recommended-mod-combinations",
-		category = "wiki-py",
-		indent = 2,
-		order = "bb",
-		starting_status = "suggested",
-	},
-	{
-		type = "tips-and-tricks-item",
-		name = "recommended-other-mods",
-		category = "wiki-py",
-		indent = 2,
-		order = "bb",
-		starting_status = "suggested",
-	},
-    ]]--
-	--[[
-	{
-		type = "tips-and-tricks-item",
-		name = "general-py-tips",
-		category = "wiki-py",
-		indent = 1,
-		order = "c",
-		starting_status = "suggested",
-		is_title = true,
-	},
-	]]--
     {
 		type = "tips-and-tricks-item",
 		name = "victory",
 		category = "wiki-py",
-		indent = 2,
-		order = "ca",
-		starting_status = "suggested",
+		indent = 1,
+		order = "c",
+        tag = '[technology=pyrrhic]',
 	},
-	--[[
-	{
-		type = "tips-and-tricks-item",
-		name = "themes",
-		category = "wiki-py",
-		indent = 2,
-		order = "cb",
-		starting_status = "suggested",
-	},
-	]]--
-	--[[
-	{
-		type = "tips-and-tricks-item",
-		name = "general-tips",
-		category = "wiki-py",
-		indent = 2,
-		order = "cc",
-		starting_status = "suggested",
-	},
-	]]--
-	--[[
-	{
-		type = "tips-and-tricks-item",
-		name = "py-technologies",
-		category = "wiki-py",
-		indent = 2,
-		order = "cd",
-		starting_status = "suggested",
-	},
-    ]]--
 	{
 		type = "tips-and-tricks-item",
 		name = "coalprocessing",
 		category = "wiki-py",
 		indent = 1,
 		order = "d",
-		starting_status = "suggested",
 		is_title = true,
-	},
-	{
-		type = "tips-and-tricks-item",
-		name = "mechanical-inserter",
-		category = "wiki-py",
-		indent = 2,
-		order = "da",
-		dependencies = {"coalprocessing"}
 	},
 	{
 		type = "tips-and-tricks-item",
 		name = "ash",
 		category = "wiki-py",
 		indent = 2,
+		order = "da",
+        tag = '[item=ash]',
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "mechanical-inserter",
+		category = "wiki-py",
+		indent = 2,
 		order = "db",
-		dependencies = {"coalprocessing"}
+        tag = '[item=burner-inserter]',
+	},
+	{
+		type = "tips-and-tricks-item",
+		name = "assembling-machine",
+		category = "wiki-py",
+		indent = 2,
+		order = "dc",
+        tag = '[item=assembling-machine-1]',
 	},
 	{
 		type = "tips-and-tricks-item",
 		name = "tailings-pond",
 		category = "wiki-py",
 		indent = 2,
-		order = "dc",
-		dependencies = {"coalprocessing"}
-	},
-	--[[
-	{
-		type = "tips-and-tricks-item",
-		name = "early-game-tips",
-		category = "wiki-py",
-		indent = 2,
 		order = "dd",
-		dependencies = {"coalprocessing"}
+        tag = '[entity=tailings-pond]',
 	},
-	]]--
-
-	--[[
-		TODO: combustion mixture section needs rewritten as indivisual pieces
-	{
-		type = "tips-and-tricks-item",
-		name = "combustion-mixture",
-		category = "wiki-py",
-		indent = 2,
-		order = "de",
-		dependencies = {"coalprocessing"},
-		trigger =
-		{
-			type = "research",
-			technology = "energy-1"
-		},
-	}
-	]]--
 })
-
---[[
-if not mods["pyalienlife"] then
+if not mods["pyalternativeenergy"] then
     data:extend(
     {
         {
             type = "tips-and-tricks-item",
-            name = "your-first-science-pack",
+            name = "combustion-mixture",
             category = "wiki-py",
             indent = 2,
-            order = "df",
-            dependencies = {"coalprocessing"},
+            order = "de",
+            tag = '[fluid=combustion-mixture1]',
+            trigger =
+            {
+                type = "unlock-recipe",
+                recipe = "gasturbinemk01"
+            },
         }
     })
 end
-]]--
