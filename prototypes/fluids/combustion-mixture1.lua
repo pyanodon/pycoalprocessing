b6cb79485b27d18bd5d58a02e6798961379027a1
notnotmelon <zacharypicco@gmail.com>
2023-10-18 16:39:57 -0500

    https://github.com/pyanodon/pybugreports/issues/319

--- prototypes/fluids/combustion-mixture1.lua
+++ prototypes/fluids/combustion-mixture1.lua
@@ -3,7 +3,7 @@ FLUID {
     name = "combustion-mixture1",
     icon = "__pycoalprocessinggraphics__/graphics/icons/combustion-mixture1.png",
     icon_size = 32,
-    default_temperature = 15,
+    default_temperature = 10,
     heat_capacity = "2KJ",
     base_color = {r = 0.811, g = 0.325, b = 0.0},
     flow_color = {r = 1, g = 1, b = 1},
