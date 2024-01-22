376ae7f58f28aeb40191246d3f30a91894ef619e
notnotmelon <zacharypicco@gmail.com>
2024-01-22 13:50:27 -0600

    Kill the ninjas

---
+++ migrations/2.1.16.lua
@@ -0,0 +1,11 @@
+local killed_ninjas = 0
+for _, surface in pairs(game.surfaces) do
+    for _, ninja in pairs(surface.find_entities_filtered{type = 'tree', name = 'ninja-tree'}) do
+        ninja.destroy()
+        killed_ninjas = killed_ninjas + 1
+    end
+end
+
+if killed_ninjas > 0 then
+    game.print('Killed ' .. killed_ninjas .. ' ninjas')
+end
