9cfb916f73f90383aa4556209167064adedb8fe6
przemo1232 <79700515+przemo1232@users.noreply.github.com>
2024-09-21 21:53:49 +0200

    fix resource autoplace

--- prototypes/ores/niobium.lua
+++ prototypes/ores/niobium.lua
@@ -1,5 +1,7 @@
 local resource_autoplace = require 'resource-autoplace'
 
+data.raw.planet.nauvis.map_gen_settings.autoplace_controls["niobium"] = {}
+
 data:extend{{
     type = "autoplace-control",
     name = "niobium",
