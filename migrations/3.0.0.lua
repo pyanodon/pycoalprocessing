dd5232f093802a7bbb559de13af80bb0d48684c5
oorzkws <65210810+oorzkws@users.noreply.github.com>
2024-10-04 14:27:43 -0600

    fix fawogae default pipes to match graphics
    migration included to rotate the entities

--- migrations/3.0.0.lua
+++ migrations/3.0.0.lua
@@ -1,1 +1,16 @@
 storage.on_tick = {}
+-- fix rotation of fawogae plantations after fixing the graphics
+for _, surface in pairs(game.surfaces) do
+    for i=1, 4 do
+        local target = 'fawogae-plantation-mk0' .. i
+        if not prototypes.entity[target] then
+            break
+        end
+        for _, plantation in pairs(surface.find_entities_filtered{name = target}) do
+            if plantation.valid then
+                local result = plantation.rotate{reverse = true}
+                log("Rotating " .. plantation.unit_number .. " was " .. (result and "successful" or "unsuccessful"))
+            end
+        end
+    end
+end
