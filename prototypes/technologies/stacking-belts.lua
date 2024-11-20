7ce870d43428e299d73cff142eb6d63c51f903b8
notnotmelon <zacharypicco@gmail.com>
2024-11-20 08:35:20 -0600

    Stack inserter balancing

--- prototypes/technologies/stacking-belts.lua
+++ prototypes/technologies/stacking-belts.lua
@@ -39,13 +39,14 @@ data:extend {py.merge(data.raw.inserter["bulk-inserter"], {
     pickup_position = {0, -1.5},
     collision_box = {{-0.15, -0.65}, {0.15, 0.65}},
     selection_box = {{-0.4, -0.9}, {0.4, 0.9}},
-    energy_per_movement = "100kJ",
-    energy_per_rotation = "100kJ",
+    energy_per_movement = "102.564102564kJ",
+    energy_per_rotation = "102.564102564kJ",
     energy_source = {
         type = "electric",
-        usage_priority = "secondary-input"
+        usage_priority = "secondary-input",
+        drain = "800kW",
     },
-    rotation_speed = data.raw.inserter.inserter.rotation_speed * 1.5,
+    rotation_speed = data.raw.inserter.inserter.rotation_speed * 1.7,
     minable = {
         mining_time = 0.2,
         result = "py-stack-inserter"
@@ -101,16 +102,21 @@ RECIPE {
     name = "py-stack-inserter",
     enabled = false,
     result = "py-stack-inserter",
-    ingredients = {},
-}:add_ingredient {"electronic-circuit", 4}:add_ingredient {"small-parts-01", 10}:add_ingredient {"intermetallics", 3}:add_ingredient {"belt", 3}:add_ingredient {"engine-unit", 3}:add_ingredient {type = "fluid", name = "lubricant", amount = 15}
+    ingredients = {
+        {type = "item", name = "fast-inserter", amount = 3},
+        {type = "item", name = "electronic-circuit", amount = 5},
+    },
+    energy_required = 2,
+}:add_ingredient {"small-parts-01", 10}:add_ingredient {"belt", 3}:add_ingredient {"engine-unit", 3}:add_ingredient {type = "fluid", name = "lubricant", amount = 200}:add_ingredient{"nbfe-alloy", 10}
 
 local i = 1
 for science, pack in pairs(science_packs_that_unlock_belt_stacking) do
     if not data.raw.technology[science] then goto continue end
 
-    local prerequisites = {science}
     if i > 1 then
         prerequisites = {science, "py-transport-belt-capacity-" .. (i - 1)}
+    else
+        prerequisites = {"fast-inserter"}
     end
 
     local tech = {
