40c04ea550c36adcccd5c32fe7343f169da57b79
notnotmelon <zacharypicco@gmail.com>
2024-11-23 21:33:39 -0600

    Added seamless migrations between loaderless machines.

--- prototypes/buildings/soil-extractor.lua
+++ prototypes/buildings/soil-extractor.lua
@@ -119,6 +119,16 @@ for i = 1, 4 do
             apparent_volume = 2.5
         },
     }
+    
+    -- https://github.com/pyanodon/pycoalprocessing/commit/cf03a35fa5dec4c74a464d847b4e266394317726
+    if not mods["pystellarexpedition"] then
+        local legacy = table.deepcopy(data.raw["assembling-machine"][name])
+        legacy.vector_to_place_result = nil
+        legacy.name = name .. "-legacy"
+        legacy.hidden = true
+        legacy.localised_name = {"", {"entity-name." .. name}, " (Legacy)"}
+        data:extend {legacy}
+    end
 end
 
 RECIPE {
