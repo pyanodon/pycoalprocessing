d7b8af03aa77ced6217940999819369c196ce2bf
Zachary Picco <zacharypicco@gmail.com>
2025-04-30 13:35:03 -0500

    Adjusted the auto-output location for sand extractors to not be in the center of a tile.

--- prototypes/buildings/sand-extractor.lua
+++ prototypes/buildings/sand-extractor.lua
@@ -40,7 +40,7 @@ for i = 1, 4 do
         name = name,
         icon = icon,
         icon_size = icon_size,
-        vector_to_place_result = {0.0, 3.01},
+        vector_to_place_result = {-0.5, 3.1},
         flags = {"placeable-neutral", "player-creation"},
         minable = {mining_time = 1, result = name},
         fast_replaceable_group = "sand-extractor",
