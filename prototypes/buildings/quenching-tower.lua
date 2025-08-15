399744bae3cb523f676541907c18c8d68a7a3f1c
Majildian <31296540+Majildian@users.noreply.github.com>
2025-08-15 16:54:37 -0700

    Adjust pipe picture positions

--- prototypes/buildings/quenching-tower.lua
+++ prototypes/buildings/quenching-tower.lua
@@ -105,35 +105,35 @@ for i = 1, 4 do
         fluid_boxes = {
             {
                 production_type = "input",
-                pipe_picture = py.pipe_pictures("assembling-machine-3", {1.08, 4.0}, {-0.82, -4.0}, nil, nil, pipe_picture_override),
+                pipe_picture = py.pipe_pictures("assembling-machine-3", {-0.82, 4.0}, {1.08, -4.0}, nil, nil, pipe_picture_override),
                 pipe_covers = py.pipe_covers(false, true, true, true),
                 volume = 1000,
                 pipe_connections = {{flow_direction = "input", position = {3.0, -1.0}, direction = defines.direction.east}}
             },
             {
                 production_type = "input",
-                pipe_picture = py.pipe_pictures("assembling-machine-3", {1.08, 4.0}, {-0.82, -4.0}, nil, nil, pipe_picture_override),
+                pipe_picture = py.pipe_pictures("assembling-machine-3", {-0.82, 4.0}, {1.08, -4.0}, nil, nil, pipe_picture_override),
                 pipe_covers = py.pipe_covers(false, true, true, true),
                 volume = 1000,
                 pipe_connections = {{flow_direction = "input", position = {3.0, 1.0}, direction = defines.direction.east}}
             },
             {
                 production_type = "output",
-                pipe_picture = py.pipe_pictures("assembling-machine-3", {1.08, 4.0}, {-0.82, -4.0}, nil, nil, pipe_picture_override),
+                pipe_picture = py.pipe_pictures("assembling-machine-3", {-0.82, 4.0}, {1.08, -4.0}, nil, nil, pipe_picture_override),
                 pipe_covers = py.pipe_covers(false, true, true, true),
                 volume = 100,
                 pipe_connections = {{flow_direction = "output", position = {-3.0, -1.0}, direction = defines.direction.west}}
             },
             {
                 production_type = "output",
-                pipe_picture = py.pipe_pictures("assembling-machine-3", {1.08, 4.0}, {-0.82, -4.0}, nil, nil, pipe_picture_override),
+                pipe_picture = py.pipe_pictures("assembling-machine-3", {-0.82, 4.0}, {1.08, -4.0}, nil, nil, pipe_picture_override),
                 pipe_covers = py.pipe_covers(false, true, true, true),
                 volume = 100,
                 pipe_connections = {{flow_direction = "output", position = {-3.0, 1.0}, direction = defines.direction.west}}
             },
             {
                 production_type = "output",
-                pipe_picture = py.pipe_pictures("assembling-machine-3", {1.08, 4.0}, {-0.82, -4.0}, nil, nil, pipe_picture_override),
+                pipe_picture = py.pipe_pictures("assembling-machine-3", {-0.82, 4.0}, {1.08, -4.0}, nil, nil, pipe_picture_override),
                 pipe_covers = py.pipe_covers(false, true, true, true),
                 volume = 100,
                 pipe_connections = {{flow_direction = "output", position = {-1.0, 3.0}, direction = defines.direction.south}}
